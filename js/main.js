c95fa09a81b68cb5944a1dbfb1c723ab1a5bd4ef
Huseyin Alper Karadeniz <halperkk@gmail.com>
2021-02-09 17:05:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-$.getJSON("test.json", function(json) {
+$.getJSON("https://raw.githubusercontent.com/Halperk/suanalytics/main/data/data_202001.json", function(json) {
     console.log(json); // this will show the info it in firebug console
 });
